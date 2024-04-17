bcbf15be7d3498627ad49d50f563cff6f7a74f8b
Sandani Muthunayaka <muthunayakahst.21@uom.lk>
2024-04-17 14:07:06 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World from <code>src/App.js</code> file. Me too
+          Testing
         </p>
         <a
           className="App-link"
